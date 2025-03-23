752515889e895a9997a90a595f4eef1b3f4872f4
Daniel Warfield <danielwarfield1@gmail.com>
2025-03-23 10:30:07 -0500

    initial commit

---
+++ client.py
@@ -0,0 +1,40 @@
+from mcp import ClientSession, StdioServerParameters, types
+from mcp.client.stdio import stdio_client
+
+# Create server parameters for stdio connection
+server_params = StdioServerParameters(
+    command="mcp",  # Executable
+    args=["run", "server.py"], 
+)
+
+
+async def run():
+    async with stdio_client(server_params) as (read, write):
+        async with ClientSession(
+            read, write
+        ) as session:
+            
+            # Initialize the connection
+            await session.initialize()
+
+            # List available tools
+            response = await session.list_tools()
+            print('\n\ntools:')
+            print(response.tools)
+
+            # List available resources
+            response = await session.list_resources()
+            print('\n\resources:')
+            print(response.resources)
+
+            # List available prompts
+            response = await session.list_prompts()
+            print('\n\resources:')
+            print(response.prompts)
+
+            
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(run())
---
+++ server.py
@@ -0,0 +1,34 @@
+from mcp.server.fastmcp import FastMCP
+import mcp.types as types
+from mcp.types import AnyUrl
+
+mcp = FastMCP("server")
+
+# ==================== Tool ===================
+@mcp.tool()
+def add(a: float, b: float) -> float:
+    """adds two numbers together.
+    
+    Args:
+        a: a number
+        b: a number
+    """
+    return a+b
+
+
+# ==================== Resources ===================
+@mcp.resource("file://pi.txt")
+async def read_resource() -> str:
+    with open('pi.txt', 'r', encoding='utf-8') as file:
+        pi_digits = file.read()
+    return pi_digits
+
+
+# ==================== Prompts ===================
+
+@mcp.prompt()
+def add_to_last(number:float) -> str:
+    """Creates a prompt that will encourage the agent to use proper tooling
+    to add a number to the final provided digit of Pi."""
+
+    return f"read the pi resource and add {number} to the final digit using the add tool."
